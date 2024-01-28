3fe59b9fd97c13d09d6dc3adac7d38b514c230c2
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-01-28 19:22:38 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClickChange = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Reactの復習</h1>
+      <p>{count}</p>
+      <button onClick={onClickChange}>カウントアップ</button>
     </div>
   );
 }
